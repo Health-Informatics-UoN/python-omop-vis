201443d9cd1d41ae67ae84a8047c4a657fc42283
James Mitchell-White <jamesjimitchell@gmail.com>
2026-09-04 15:28:13 +0100

    Create reconcile_types.py
    I wanted to make a nice mapping between R and python types, but won't need it.

---
+++ src/summarised_result/reconcile_types.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class RType(Enum):
+    CHARACTER = ("character", str)
+    INT = ("int", int)
+
+    py_type: type
+
+    def __new__(cls, value: str, py_type: type):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.py_type = py_type
+
+        return obj
